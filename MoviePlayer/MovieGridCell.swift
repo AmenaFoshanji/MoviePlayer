ecb3f22ea0e102e6b5ff54aafe1a5bf2661d1ad3
Amena <amena.foshanji@gmail.com>
2020-09-21 21:02:31 -0400

    Part two class Assignment

---
+++ MoviePlayer/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  MoviePlayer
+//
+//  Created by Arash Foshanji on 9/21/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
